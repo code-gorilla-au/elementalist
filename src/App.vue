38f1e97c42771ecc29f8ee3b43ebd5b78db6b404
mauricio riquelme <m.riquelme223@gmail.com>
2021-06-14 11:18:33 +1000

    adding some stuff

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,14 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3 + TypeScript + Vite" />
+  <h1>Elementalist</h1>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue'
-import HelloWorld from './components/HelloWorld.vue'
+import { defineComponent } from 'vue';
 
 export default defineComponent({
   name: 'App',
-  components: {
-    HelloWorld
-  }
-})
+  components: {},
+});
 </script>
 
 <style>
@@ -21,5 +17,4 @@ body {
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
 }
-
 </style>
---
+++ src/components/Game.vue
@@ -0,0 +1,12 @@
+<template>
+  <div id="Game" />
+</template>
+
+<script>
+import { defineComponent } from 'vue';
+export default defineComponent({
+  name: 'Game',
+});
+</script>
+
+<style></style>
